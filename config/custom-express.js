6fce7a9685cfac64c86b3882e44065d85e428275
Brad Hick <bradleyhrm@gmail.com>
2017-05-01 16:10:54 -0300

    Instalação do MySql

--- config/custom-express.js
+++ config/custom-express.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 module.exports = function() {
   var app = express();
 
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
   consign()
       .include('controllers')
       .into(app);
